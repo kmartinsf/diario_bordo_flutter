372ed530e2ac9831e238d09849311ec3c2313b2d
Kaiena <kaienarmartins@gmail.com>
2025-08-04 20:04:39 -0300

    refactor: update CustomInput to support error and suffix icon

--- lib/presentation/widgets/input.dart
+++ lib/presentation/widgets/input.dart
@@ -5,6 +5,9 @@ class CustomInput extends StatelessWidget {
   final bool obscure;
   final TextEditingController? controller;
   final String? Function(String?)? validator;
+  final String? errorText;
+  final Widget? suffixIcon;
+  final bool hasLoginError;
 
   const CustomInput({
     super.key,
@@ -12,6 +15,9 @@ class CustomInput extends StatelessWidget {
     this.obscure = false,
     this.controller,
     this.validator,
+    this.errorText,
+    this.suffixIcon,
+    this.hasLoginError = false,
   });
 
   @override
@@ -23,14 +29,24 @@ class CustomInput extends StatelessWidget {
       decoration: InputDecoration(
         hintText: hint,
         filled: true,
-        fillColor: const Color(0xFFF5F5F5),
-        contentPadding: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 20,
-        ),
+        fillColor: hasLoginError
+            ? const Color(0xFFFDECEC)
+            : const Color(0xFFF5F5F5),
+        errorText: errorText,
+        contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 4),
+        prefix: const Padding(padding: EdgeInsets.only(left: 6)),
+        suffixIcon: suffixIcon,
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
           borderSide: BorderSide.none,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Colors.red, width: 1.5),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Colors.red, width: 1.5),
         ),
       ),
     );
