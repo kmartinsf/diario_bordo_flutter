c811672e56dba25374190470ab06d55fc53a49e8
Kaiena <kaienarmartins@gmail.com>
2025-08-04 14:20:31 -0300

    feat: implement user registration with Firebase Auth and Firestore

---
+++ lib/data/services/firebase/auth_service.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> register({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    UserCredential result = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    await _firestore.collection('users').doc(result.user!.uid).set({
+      'name': name,
+      'email': email,
+      'createdAt': Timestamp.now(),
+    });
+  }
+}
