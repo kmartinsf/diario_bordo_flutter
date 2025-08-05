7bc96d69508b6994624ff81b6209e080152269ab
Kaiena <kaienarmartins@gmail.com>
2025-08-05 14:07:31 -0300

    chore: load .env file using flutter_dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:diario_bordo_flutter/presentation/screens/home.dart';
+import 'package:diario_bordo_flutter/presentation/screens/home_screen.dart';
+import 'package:diario_bordo_flutter/presentation/screens/new_diary_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'firebase_options.dart';
 import 'presentation/screens/login_screen.dart';
 import 'presentation/screens/register_screen.dart';
@@ -10,10 +13,9 @@ import 'presentation/screens/welcome_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await dotenv.load(fileName: ".env");
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   runApp(const ProviderScope(child: BoardingDiaryApp()));
 }
 
