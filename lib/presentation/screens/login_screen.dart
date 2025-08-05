f1408b72df13661000778a47bf78dc16f564a5cc
Kaiena <kaienarmartins@gmail.com>
2025-08-05 14:05:53 -0300

    refactor: update auth service import path

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -1,5 +1,5 @@
-import 'package:diario_bordo_flutter/data/services/auth_providers.dart';
 import 'package:diario_bordo_flutter/presentation/widgets/input.dart';
+import 'package:diario_bordo_flutter/providers/auth_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
