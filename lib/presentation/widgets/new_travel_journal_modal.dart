648aef2feb0b1322e243dd80a9baf84a0274cc05
Kaiena <kaienarmartins@gmail.com>
2025-08-06 10:23:05 -0300

    refactor: replace withOpacity with withValues for box shadow color

--- lib/presentation/widgets/new_travel_journal_modal.dart
+++ lib/presentation/widgets/new_travel_journal_modal.dart
@@ -216,7 +216,7 @@ class _NewTravelJournalModalState extends ConsumerState<NewTravelJournalModal> {
                                       borderRadius: BorderRadius.circular(12),
                                       boxShadow: [
                                         BoxShadow(
-                                          color: Colors.black.withOpacity(0.1),
+                                          color: Colors.black.withValues(alpha: 0.1),
                                           blurRadius: 6,
                                           offset: const Offset(0, 2),
                                         ),
