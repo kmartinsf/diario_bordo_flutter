2d1806e2138bda2b520409ce975202d5a692de1f
Kaiena <kaienarmartins@gmail.com>
2025-08-07 10:50:11 -0300

    feat: add provider for AuthRepository

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,4 +1,9 @@
+import 'package:diario_bordo_flutter/data/repositories/auth_repository.dart';
 import 'package:diario_bordo_flutter/data/services/firebase/auth_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authServiceProvider = Provider<AuthService>((ref) => AuthService());
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  return AuthRepository();
+});
