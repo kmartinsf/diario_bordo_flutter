b3d5e636cce8d5fbba4a35275d00a285d027518d
Kaiena <kaienarmartins@gmail.com>
2025-08-07 11:03:40 -0300

    refactor: generalize confirmation modal and update journal actions

--- lib/presentation/widgets/journal_options_menu.dart
+++ lib/presentation/widgets/journal_options_menu.dart
@@ -1,11 +1,14 @@
 import 'dart:ui';
 
 import 'package:diario_bordo_flutter/data/models/travel_journal_model.dart';
-import 'package:diario_bordo_flutter/presentation/widgets/delete_journal_modal.dart';
+import 'package:diario_bordo_flutter/presentation/widgets/confirmation_journal_modal.dart';
+import 'package:diario_bordo_flutter/presentation/widgets/custom_snackbar.dart';
 import 'package:diario_bordo_flutter/presentation/widgets/travel_journal_modal.dart';
+import 'package:diario_bordo_flutter/providers/travel_journal_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class JournalOptionsMenu extends StatelessWidget {
+class JournalOptionsMenu extends ConsumerWidget {
   final TravelJournal journal;
   final VoidCallback onRefresh;
 
@@ -16,7 +19,7 @@ class JournalOptionsMenu extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Material(
       type: MaterialType.transparency,
       child: PopupMenuButton<String>(
@@ -36,14 +39,30 @@ class JournalOptionsMenu extends StatelessWidget {
               ),
               builder: (context) => BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
-                child: TravelJournalModal(journal: journal, onSuccess: onRefresh),
+                child: TravelJournalModal(
+                  journal: journal,
+                  onSuccess: onRefresh,
+                ),
               ),
             );
           } else if (value == 'delete') {
-            await deleteConfirmation(
-              context,
-              journalId: journal.id,
-              onDeleteSuccess: onRefresh,
+            await confirmationModal(
+              context: context,
+              title: 'Excluir diário',
+              message:
+                  'Todas as informações registradas nele serão perdidas de forma definitiva.',
+              confirmText: 'Excluir diário',
+              cancelText: 'Cancelar',
+              confirmTextColor: Colors.red,
+              onConfirm: () async {
+                await ref
+                    .read(travelJournalNotifierProvider.notifier)
+                    .deleteJournal(journal.id);
+                onRefresh();
+                if (context.mounted) {
+                  customSnackBar(context, 'Diário removido');
+                }
+              },
             );
           }
         },
@@ -53,7 +72,11 @@ class JournalOptionsMenu extends StatelessWidget {
             height: 24,
             child: Text(
               'Editar diário',
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.grey),
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey,
+              ),
             ),
           ),
           const PopupMenuDivider(),
@@ -62,7 +85,11 @@ class JournalOptionsMenu extends StatelessWidget {
             height: 24,
             child: Text(
               'Excluir diário',
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.red),
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: Colors.red,
+              ),
             ),
           ),
         ],
